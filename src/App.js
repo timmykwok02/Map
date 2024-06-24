135ffc027313860c6edeaa37f04e2319df228c63
Timmy <timmykwok02@gmail.com>
2024-06-23 17:20:20 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import {Icon} from 'leaflet';
 
 function App() {
+  // Setting marker on KPU Library on map
+  const markers = [{
+    geocode: [49.133129, -122.871460],
+    popUp: "KPU Surrey Library"
+    }];
+
+  // Configuring custom icons
+  const customIcon = new Icon({
+    iconUrl: "https://1000logos.net/wp-content/uploads/2021/02/KPU-logo.png",
+    // iconUrl: require("./img/KPUicon.png"),
+    iconSize: [64, 40] 
+    })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Coordinates of KPU Library as the centre of the map
+    // Getting map image from openstreetmap
+    <MapContainer center={[49.133129, -122.871460]} zoom={18}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {markers.map(marker => (
+        <Marker position={marker.geocode} icon={customIcon}>
+          <Popup>{marker.popUp}</Popup>
+        </Marker>
+      ))}
+    </MapContainer>
   );
 }
 
